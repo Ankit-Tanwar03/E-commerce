a7881ffa027c9f2bce8526a4bf5bec55ada2c418
Ankit Tanwar <109900657+Ankit-Tanwar03@users.noreply.github.com>
2024-02-06 13:15:18 +0000

    Forgot Password functionality added to schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import authRoles from "../utils/authRoles";
+import JWT  from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import config from "../config/index";
 
 const userSchema = new mongoose.Schema(
     {
@@ -38,9 +42,50 @@ const userSchema = new mongoose.Schema(
 
 //password encryption
 userSchema.pre("save", async function (next){
-    if(!this.modified("password")) return next();
+    if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10)
     next();
 })
+
+//adding more features directly into the schema
+userSchema.methods = {
+    //comparing passwords
+    comparePassword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword, this.password)     //this.password is password from backend and enteredPaasword is password coming from frontend
+    },
+
+    //generating JWT
+    getJwtToken: function(){
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn: config.JWT_EXPIRY
+            }
+            
+            )
+    },
+
+    //forgot password
+    generateForgotPasswordToken: function()
+    {
+        const forgotToken = crypto.randomBytes(20).toString('hex');
+
+        //step-1 save to db
+        this.forgotPasswordToken = crypto
+        .createHash('SHA256')
+        .update(forgotToken)
+        .digest('hex')
+
+        this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
+
+        //step-2 send to user
+        return forgotToken
+    }
+
+}
 
 export default mongoose.model("User", userSchema)
