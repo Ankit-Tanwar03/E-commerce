796033e2eb13d3dddc349b7f5fe80a9a1b47fc29
Ankit Tanwar <109900657+Ankit-Tanwar03@users.noreply.github.com>
2024-02-07 16:06:13 +0000

    added morgan and express middleware

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express()
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(cors())
+app.use(morgan('tiny')) //logs api req and res
 
 export default app;
 
