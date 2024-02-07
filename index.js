62af96200a5e0651a107d4ce26dda5d738567ca4
Ankit Tanwar <109900657+Ankit-Tanwar03@users.noreply.github.com>
2024-02-07 15:47:23 +0000

    DB Connection with iffe

---
+++ index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+(async => {
+    try {
+        await mongoose.connect(config.MONGODB_URL){
+            console.log("DB Connected");
+
+            app.on('error', (err) => {
+                console.log(err);
+                throw err
+            })    
+            
+            app.listen(config.PORT, () => {
+                console.log(`Listening on Port ${config.PORT}`);
+
+            })
+        }
+        
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+})()
