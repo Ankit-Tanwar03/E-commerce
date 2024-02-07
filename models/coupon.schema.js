537ebeb5bd55638467db98d5286864dd553f6a81
Ankit Tanwar <109900657+Ankit-Tanwar03@users.noreply.github.com>
2024-02-07 11:32:08 +0000

    Coupon Schema added

---
+++ models/coupon.schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+    {
+        code: {
+            String,
+            required: [true, "Please enter the coupon code"]
+        },
+
+        discount: {
+            Number,
+            default: 0
+        },
+    
+        isActive: {
+            Boolean,
+            default: true
+        },
+    },
+    {
+        timestamps: true
+    }
+    
+)
+
+export default mongoose.Schema("Coupon", couponSchema)
